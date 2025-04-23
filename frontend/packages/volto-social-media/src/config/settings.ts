52d31d3ed43229164bef28fcd3c768aaae559c0a
Victor Fernandez de Alba <sneridagh@gmail.com>
2025-04-23 11:38:26 +0200

    Update to latest Volto, fix settings

--- frontend/packages/volto-social-media/src/config/settings.ts
+++ frontend/packages/volto-social-media/src/config/settings.ts
@@ -1,16 +1,36 @@
 import type { ConfigType } from '@plone/registry';
 
+type apiExpanderInherit = {
+  match: string;
+  GET_CONTENT: string[];
+  querystring:
+    | { [key: string]: string }
+    | ((
+        config,
+        querystring: { config: ConfigType; querystring: object },
+      ) => { [key: string]: string });
+};
+
 export default function install(config: ConfigType) {
-  // Expanders
   config.settings.apiExpanders = [
     ...config.settings.apiExpanders,
     {
       match: '',
       GET_CONTENT: ['inherit'],
-      querystring: {
-        'expand.inherit.behaviors': 'plonegovbr.socialmedia.settings',
+      querystring: (config, querystring) => {
+        if (querystring['expand.inherit.behaviors']) {
+          return {
+            'expand.inherit.behaviors': querystring[
+              'expand.inherit.behaviors'
+            ].concat(',', 'plonegovbr.socialmedia.settings'),
+          };
+        } else {
+          return {
+            'expand.inherit.behaviors': 'plonegovbr.socialmedia.settings',
+          };
+        }
       },
-    },
+    } as apiExpanderInherit,
   ];
   return config;
 }
