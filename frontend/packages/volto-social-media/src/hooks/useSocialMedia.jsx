21b28ed0fc2e62569bed6962b3e06726ec4d30b3
Víctor Fernández de Alba <sneridagh@gmail.com>
2025-06-04 17:23:02 +0200

    Fixed Redux complaining about returining a different object on every rerender. (#6)
    * Fixed Redux complaining about returining a different object on every rerender.
    
    * Changelog
    
    * Update Volto and VLT

--- frontend/packages/volto-social-media/src/hooks/useSocialMedia.jsx
+++ frontend/packages/volto-social-media/src/hooks/useSocialMedia.jsx
@@ -1,17 +1,19 @@
 import { useSelector } from 'react-redux';
 
+const DEFAULT_SOCIAL_MEDIA_SETTINGS = {
+  share_social_data: true,
+  facebook_app_id: '',
+  facebook_username: '',
+  x_username: '',
+  social_links: [],
+};
+
 export const useSocialMedia = () => {
   const settings = useSelector(
     (state) =>
       state.content?.data?.['@components']?.inherit?.[
         'plonegovbr.socialmedia.settings'
-      ]?.data || {
-        share_social_data: true,
-        facebook_app_id: '',
-        facebook_username: '',
-        x_username: '',
-        social_links: [],
-      },
+      ]?.data || DEFAULT_SOCIAL_MEDIA_SETTINGS,
   );
   return settings;
 };
